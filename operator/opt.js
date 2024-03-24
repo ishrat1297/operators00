877222aaceb2fbb190cdd4829ad395ada2d3b838
afshan feroz <160025739+ishrat1297@users.noreply.github.com>
2024-03-24 08:06:07 +0500

    Add files via upload
    "Operator" is a collection of mathematical operator implementations in various programming languages. It provides ready-to-use functions for basic arithmetic operations and advanced mathematical calculations.

---
+++ operator/opt.js
@@ -0,0 +1,112 @@
+
+// OPERATOR
+// ADDITION OPERATOR
+// const num1=1
+// const num2=2
+// console.log(num1+num2)
+// const num11=3
+// const num22=4
+// const sum=num11+num22
+// console.log(sum)
+// console.log(4+5)
+// const num111=6
+// const num222=7
+// const num333=num111+num222
+// console.log(num333+8)
+// const numA=8
+// const numB=9
+// console.log(numA+numB)
+// const numC=10
+// const numD=11
+// const SUM=numC+numD
+// console.log(SUM)
+// console.log(11+12)
+// const numE=13
+// const numF=14
+// console.log(numE+numF+15)
+// const numG=15
+// const numH=16
+// console.log(numG+numH+17+18+19+20+21)
+// // SUBTRACTION
+// const numI = 22;
+// const numj = 23;
+// console.log(numI - numj);
+// const numk = 24;
+// const numl = 25;
+// const numm = numk - numl;
+// console.log(numm);
+// const numn = 26;
+// const numo = 27;
+// console.log(numn - numo + 46 - 78);
+// console.log(34 - 87 - 76 - 987);
+// // multiplication
+// const num11 = 67;
+// const num22 = 76;
+// console.log(num11 * num22);
+// const num33 = 66;
+// const num44 = 88;
+// const num55 = num33 * num44 * 77;
+// console.log(num55);
+// console.log(89 * 988 * 56);
+// const num66 = 99;
+// var num88 = 78;
+// console.log(num66 * num88 + 65);
+// // DIVISION
+// const num09 = 45;
+// const num78 = 89;
+// console.log(num09 / num78);
+// // REMAINDER
+// const num880 = 25;
+// const num870 = 3;
+// console.log(num880 % num870);
+// // EXPONENTION
+// const num1 = 4;
+// const num2 = 3;
+// console.log(num1 ** num2);
+// // COMPARISION OPERATOR
+// // is greater than
+// const num12 = 6;
+// const num_12 = 8;
+// console.log(num12 > num_12);
+// // is less than
+// const Num13 = 9;
+// const num98 = 7;
+// console.log(Num13 < num98);
+// // is equal to
+// const num90 = 78;
+// const num987 = 78;
+// console.log(num90 == num987);
+// // is less than and equal to
+// const num4 = 45;
+// const num5 = 67;
+// console.log(num4 <= num5);
+// const num6 = 78;
+// const num7 = 97;
+// console.log(num7 <= num6);
+// // is greater than and equal to
+// const num00 = 98;
+// const num99 = 67;
+// console.log(num00 >= num99);
+// // is not equal to
+// const num8 = 98;
+// const num9 = 98;
+// console.log(num8 !== num9);
+// // concatenation
+// const name11 = "Fatima";
+// const name33 = "zahra";
+// console.log(name11 + name33);
+// const name22 = "Fatima";
+// const name77 = "zahra";
+// console.log(name22 + " " + name77);
+// const name98 = "Dua";
+// const num990 = "01";
+// console.log(name98 + " " + num990);
+// const name97 = "Umme Aman";
+// const num991 = "02";
+// console.log(name97 + " " + num991);
+// const name92 = "Ayesha";
+// const num992 = "03";
+// console.log(name92 + " " + num992);
+// const name94 = "Umer";
+// const num998 = "04";
+// console.log(name94 + " " + num998);
